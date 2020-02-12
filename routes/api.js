c53345b97424a1f61f9deeb078eae2a507bc8c57
tianwei <464231692@qq.com>
2020-02-12 02:29:07 -0500

    distance

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,25 @@ let router = express.Router();
 let Uber = require('../models/model.js');
 
 router.get('/uber', (req,res)=>{
-    Ninja.geoNear(
-        {type:'Point', coordinates:[parseFloat(req.query.lng),parseFloat(req.query.lat)]},
-        {maxDistance:100000, spherical:true}
-    ).then(function(data){
-        res.send(data);
+    // Uber.geoNear(
+    //     {type:'Point', coordinates:[parseFloat(req.query.lng),parseFloat(req.query.lat)]},
+    //     {maxDistance:100000, spherical:true}
+    // ).then(function(data){
+    //     res.send(data);
+    // });
+    Uber.aggregate([
+        {
+            $geoNear: {
+                near: {type:'Point', coordinates:[parseFloat(req.query.lng), parseFloat(req.query.lat)]},
+                distanceField: "dist.calculated",
+                maxDistance: 100000,
+                spherical: true                
+            }
+        }
+    ]).then(function(Locs){
+        res.send(Locs)
     });
+    
 });
 
 router.post('/uber', (req,res,next)=>{
